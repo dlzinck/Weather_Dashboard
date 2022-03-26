5ce815ba486e2701bd32ece86f9cd9e8e6449aa2
Drew Zinck <dlzincktheii@gmail.com>
2022-03-26 14:42:02 -0700

    added clearElement and findUV functions, added descriptions

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,7 @@ let icon;
      }
 
 //Functions for application
+//This function is designed to retrieve the information stored and add information to the search history using an unordered list and for loop
 function loadCity() {
     cleaningElement(savedCitiesCont);
 
@@ -29,8 +30,33 @@ function loadCity() {
         }
 };
 
+//This listener is designed to show current and future weather conditions on the 'click'
 $(document).on("click", ".list-group-item", function(event) {
     event.preventDefault();
     let city = $(this).attr("attr");
     callApiFetch(city);
 });
+
+//This function is designed to clean everything inside the container
+function clearElement(element) {
+    element.innerHTML = "";
+};
+
+//This function is designed to show if the weather is favorbale, moderate, or severe by using colors based off of the UV Index
+function findUV(uv) {
+    let indexUV = parseFloat(uv);
+    let backgroundColor;
+    if(indexUV < 3){
+        backgroundColor = "bg-success";
+    }
+    else if( indexUV < 6){
+        backgroundColor = "bg-warning";
+    }
+        else if(indexUV < 8){
+                backgroundColor = "bg-danger";
+            }
+            else {
+                    backgroundColor = "bg-dark";
+                }
+    return backgroundColor;
+};
